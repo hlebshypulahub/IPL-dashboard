a554c2fae344b4687c9b7ad8aacdfe779c2b3da6
hlebs <glebszypula1997@gmail.com>
2021-09-25 00:09:05 +0200

    frontend

--- src/main/java/ipl/fullstack/ipldashboard/controller/TeamController.java
+++ src/main/java/ipl/fullstack/ipldashboard/controller/TeamController.java
@@ -3,11 +3,13 @@ package ipl.fullstack.ipldashboard.controller;
 import ipl.fullstack.ipldashboard.model.Team;
 import ipl.fullstack.ipldashboard.repository.MatchRepository;
 import ipl.fullstack.ipldashboard.repository.TeamRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private final TeamRepository teamRepository;
